53d3a1391cbd3d13b9dcd180236ab51234aa521d
arielk-koop <arielk@kooply.com>
2025-06-14 21:40:37 +0300

    Temporarily hardcode Google auth to true

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,5 @@
-// Default to true if environment variable is not set
-const envValue = import.meta.env.VITE_USE_GOOGLE_AUTH;
-export const USE_GOOGLE_AUTH = envValue === undefined ? true : envValue === 'true';
+// Temporarily hardcoded to true to ensure deployment works
+export const USE_GOOGLE_AUTH = true;
 
 
 
